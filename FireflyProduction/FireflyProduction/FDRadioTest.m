e23eb4a129016f0a625561048ac0f2a2163dbbc4
Denis Bohm <denis@fireflydesign.com>
2013-09-26 15:33:56 -0500

    improve radio test robustness

--- FireflyProduction/FireflyProduction/FDRadioTest.m
+++ FireflyProduction/FireflyProduction/FDRadioTest.m
@@ -25,6 +25,8 @@
 @property id<FDRadioTestDelegate> delegate;
 @property NSNumber *RSSI;
 @property NSDate *start;
+@property NSDate *send;
+@property NSUInteger sendRetries;
 @property CBPeripheral *peripheral;
 @property CBCharacteristic *characteristic;
 @property NSUInteger rssiCount;
@@ -34,6 +36,8 @@
 @property NSUInteger writeIndex;
 @property NSUInteger writeCount;
 @property NSMutableArray *updates;
+@property NSUInteger retries;
+@property BOOL connect;
 
 @end
 
@@ -47,6 +51,17 @@
     return self;
 }
 
+- (void)reset
+{
+    _send = nil;
+    _sendRetries = 0;
+    _characteristic = nil;
+    _writeStart = nil;
+    _writeEnd = nil;
+    _writeIndex = 0;
+    _writeCount = 0;
+}
+
 @end
 
 @interface FDRadioTest () <CBCentralManagerDelegate, CBPeripheralDelegate>
@@ -56,6 +71,9 @@
 @property NSMutableDictionary *tests;
 @property NSTimer *timer;
 @property NSTimeInterval timeout;
+@property NSTimeInterval sendTimeout;
+@property NSUInteger sendRetries;
+@property NSUInteger retries;
 @end
 
 @implementation FDRadioTest
@@ -66,6 +84,9 @@
         _tests = [NSMutableDictionary dictionary];
         _dataCharacteristicUUID = [CBUUID UUIDWithString:@"2a24"]; // Device Information: Model Number String
         _timeout = 10.0;
+        _sendTimeout = 0.5;
+        _sendRetries = 3;
+        _retries = 3;
     }
     return self;
 }
@@ -73,22 +94,50 @@
 - (void)start
 {
     _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
-    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(check:) userInfo:nil repeats:YES];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.25 target:self selector:@selector(check:) userInfo:nil repeats:YES];
+}
+
+- (void)timeout:(FDRadioTestContext *)context
+{
+    [_centralManager cancelPeripheralConnection:context.peripheral];
+    [_tests removeObjectForKey:context.name];
+    FDRadioTestResult *result = [[FDRadioTestResult alloc] init];
+    result.pass = false;
+    result.timeout = true;
+    result.count = context.writeCount;
+    result.rssi = [context.RSSI doubleValue];
+    [context.delegate radioTest:self complete:context.name result:result];
+}
+
+- (void)retry:(FDRadioTestContext *)context
+{
+    [context reset];
+    ++context.retries;
+    context.connect = YES;
 }
 
 - (void)check:(NSTimer *)timer
 {
     NSDate *now = [NSDate date];
     for (FDRadioTestContext *context in [_tests allValues]) {
-        if ((context.start != nil) && ([now timeIntervalSinceDate:context.start] > _timeout)) {
-            [_tests removeObjectForKey:context.name];
-            FDRadioTestResult *result = [[FDRadioTestResult alloc] init];
-            result.pass = false;
-            result.timeout = true;
-            result.count = context.writeCount;
-            result.rssi = [context.RSSI doubleValue];
-            [context.delegate radioTest:self complete:context.name result:result];
+        if (context.start == nil) {
+            continue;
         }
+        if (context.connect) {
+            context.connect = NO;
+            [_centralManager connectPeripheral:context.peripheral options:nil];
+            continue;
+        }
+        if ([now timeIntervalSinceDate:context.start] > _timeout) {
+            [self timeout:context];
+        } else
+            if ((context.send != nil) && ([now timeIntervalSinceDate:context.send] > _sendTimeout)) {
+                if (context.sendRetries >= _sendRetries) {
+                    [self retry:context];
+                } else {
+                    [self setCharacteristicValue:context retry:YES];
+                }
+            }
     }
 }
 
@@ -103,7 +152,11 @@
 
 - (void)cancelTest:(NSString *)name
 {
-    [_tests removeObjectForKey:name];
+    FDRadioTestContext *context = _tests[name];
+    if (context != nil) {
+        [_centralManager cancelPeripheralConnection:context.peripheral];
+        [_tests removeObjectForKey:name];
+    }
 }
 
 
@@ -164,14 +217,19 @@
     FDLog(@"didDisconnectPeripheral %@ : %@", peripheral.name, error);
     FDRadioTestContext *context = _tests[peripheral.name];
     if (context != nil) {
-        [_tests removeObjectForKey:peripheral.name];
-        FDRadioTestResult *result = [[FDRadioTestResult alloc] init];
-        result.pass = context.writeEnd != nil;
-        result.count = context.writeCount;
-        result.rssi = [context.RSSI doubleValue];
-        result.duration = [context.writeEnd timeIntervalSinceDate:context.writeStart];
-        result.updates = context.updates;
-        [context.delegate radioTest:self complete:peripheral.name result:result];
+        if ((context.writeEnd == nil) && (context.retries < _retries)) {
+            FDLog(@"unexpected disconnect, retry opening connection");
+            [self retry:context];
+        } else {
+            [_tests removeObjectForKey:peripheral.name];
+            FDRadioTestResult *result = [[FDRadioTestResult alloc] init];
+            result.pass = context.writeEnd != nil;
+            result.count = context.writeCount;
+            result.rssi = [context.RSSI doubleValue];
+            result.duration = [context.writeEnd timeIntervalSinceDate:context.writeStart];
+            result.updates = context.updates;
+            [context.delegate radioTest:self complete:peripheral.name result:result];
+        }
     }
 }
 
@@ -183,12 +241,18 @@
     }
 }
 
-- (void)setCharacteristicValue:(FDRadioTestContext *)context
+- (void)setCharacteristicValue:(FDRadioTestContext *)context retry:(BOOL)retry
 {
-    FDLog(@"setCharacteristicValue");
-    [context.peripheral readRSSI];
+    FDLog(@"setCharacteristicValue retry=%@", retry ? @"YES" : @"NO");
+//    [context.peripheral readRSSI];
     
-    ++context.writeCount;
+    context.send = [NSDate date];
+    if (retry) {
+        ++context.sendRetries;
+    } else {
+        context.sendRetries = 0;
+        ++context.writeCount;
+    }
     uint8_t byte = ((uint8_t *)context.writeData.bytes)[context.writeIndex];
     uint8_t bytes[20] = {0x01, context.writeIndex++, byte};
     [context.peripheral writeValue:[NSData dataWithBytes:bytes length:sizeof(bytes)] forCharacteristic:context.characteristic type:CBCharacteristicPropertyWrite];
@@ -204,7 +268,7 @@
                 context.characteristic = characteristic;
                 [peripheral setNotifyValue:YES forCharacteristic:characteristic];
                 context.writeStart = [NSDate date];
-                [self setCharacteristicValue:context];
+                [self setCharacteristicValue:context retry:NO];
             }
         }
     }
@@ -236,7 +300,7 @@
                 [_centralManager cancelPeripheralConnection:peripheral];
             } else {
                 if (n == context.writeCount) {
-                    [self setCharacteristicValue:context];
+                    [self setCharacteristicValue:context retry:NO];
                 }
             }
         }
