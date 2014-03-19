7e8daa88101e16db17ee6a578d8653d82301b8ee
Denis Bohm <denis@fireflydesign.com>
2014-03-18 20:47:53 -0500

    update project settings

--- FireflyProduction/FireflyProductionTests/FireflyProductionTests.h
+++ FireflyProduction/FireflyProductionTests/FireflyProductionTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Firefly Design. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface FireflyProductionTests : SenTestCase
+@interface FireflyProductionTests : XCTestCase
 
 @end
--- FireflyProduction/FireflyProductionTests/FireflyProductionTests.m
+++ FireflyProduction/FireflyProductionTests/FireflyProductionTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in FireflyProductionTests");
+    XCTFail(@"Unit tests are not implemented yet in FireflyProductionTests");
 }
 
 @end
