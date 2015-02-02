bda00a89c2ee98d52b12bde6b88a851e208c5472
Denis Bohm <denis@fireflydesign.com>
2015-02-01 19:00:48 -0600

    fixup module imports

--- FireflyProduction/FireflyProduction/FireflyProduction.h
+++ FireflyProduction/FireflyProduction/FireflyProduction.h
@@ -8,27 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
-#import "FDI2C.h"
-#import "FDNumato.h"
-#import "FDPowerSupply.h"
-#import "FDMultimeter.h"
-#import "FDSerialPort.h"
-#import "FDExecutable.h"
-#import "FDFireflyFlash.h"
-#import "FDFireflyFlashEFM32.h"
-#import "FDFireflyFlashNANO1XX.h"
-#import "FDFireflyFlashNRF51.h"
-#import "FDFireflyFlashSTM32F4.h"
-#import "FDGdbServer.h"
-#import "FDGdbServerSwd.h"
-#import "FDRadioTest.h"
-#import "FDRadioTest.h"
-#import "FDUsbTest.h"
-#import "FDBuilderView.h"
-#import "FDTargetOptionsView.h"
-#import "FDSerialWireDebugTask.h"
-#import "FDSerialWireDebugTest.h"
-#import "FDSerialWireDebugOperation.h"
+#import <FireflyProduction/FDI2C.h>
+#import <FireflyProduction/FDNumato.h>
+#import <FireflyProduction/FDPowerSupply.h>
+#import <FireflyProduction/FDMultimeter.h>
+#import <FireflyProduction/FDSerialPort.h>
+#import <FireflyProduction/FDExecutable.h>
+#import <FireflyProduction/FDFireflyFlash.h>
+#import <FireflyProduction/FDFireflyFlashEFM32.h>
+#import <FireflyProduction/FDFireflyFlashNANO1XX.h>
+#import <FireflyProduction/FDFireflyFlashNRF51.h>
+#import <FireflyProduction/FDFireflyFlashSTM32F4.h>
+#import <FireflyProduction/FDGdbServer.h>
+#import <FireflyProduction/FDGdbServerSwd.h>
+#import <FireflyProduction/FDRadioTest.h>
+#import <FireflyProduction/FDRadioTest.h>
+#import <FireflyProduction/FDUsbTest.h>
+#import <FireflyProduction/FDBuilderView.h>
+#import <FireflyProduction/FDTargetOptionsView.h>
+#import <FireflyProduction/FDSerialWireDebugTask.h>
+#import <FireflyProduction/FDSerialWireDebugTest.h>
+#import <FireflyProduction/FDSerialWireDebugOperation.h>
 
 @interface FireflyProduction : NSObject
 
