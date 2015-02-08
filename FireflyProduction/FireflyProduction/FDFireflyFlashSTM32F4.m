1e4640c6d6ffb2921b7cc47576bfd3209a85bd11
Denis Bohm <denis@fireflydesign.com>
2015-02-07 23:01:29 -0600

    add STM32 debug lock function

--- FireflyProduction/FireflyProduction/FDFireflyFlashSTM32F4.m
+++ FireflyProduction/FireflyProduction/FDFireflyFlashSTM32F4.m
@@ -56,10 +56,55 @@
 #define FLASH_CR_EOPIE                       ((uint32_t)0x01000000)
 #define FLASH_CR_LOCK                        ((uint32_t)0x80000000)
 
+/*******************  Bits definition for FLASH_OPTCR register  ***************/
+#define FLASH_OPTCR_OPTLOCK                 ((uint32_t)0x00000001)
+#define FLASH_OPTCR_OPTSTRT                 ((uint32_t)0x00000002)
+#define FLASH_OPTCR_BOR_LEV_0               ((uint32_t)0x00000004)
+#define FLASH_OPTCR_BOR_LEV_1               ((uint32_t)0x00000008)
+#define FLASH_OPTCR_BOR_LEV                 ((uint32_t)0x0000000C)
+#define FLASH_OPTCR_BFB2                    ((uint32_t)0x00000010)
+
+#define FLASH_OPTCR_WDG_SW                  ((uint32_t)0x00000020)
+#define FLASH_OPTCR_nRST_STOP               ((uint32_t)0x00000040)
+#define FLASH_OPTCR_nRST_STDBY              ((uint32_t)0x00000080)
+#define FLASH_OPTCR_RDP                     ((uint32_t)0x0000FF00)
+#define FLASH_OPTCR_RDP_0                   ((uint32_t)0x00000100)
+#define FLASH_OPTCR_RDP_1                   ((uint32_t)0x00000200)
+#define FLASH_OPTCR_RDP_2                   ((uint32_t)0x00000400)
+#define FLASH_OPTCR_RDP_3                   ((uint32_t)0x00000800)
+#define FLASH_OPTCR_RDP_4                   ((uint32_t)0x00001000)
+#define FLASH_OPTCR_RDP_5                   ((uint32_t)0x00002000)
+#define FLASH_OPTCR_RDP_6                   ((uint32_t)0x00004000)
+#define FLASH_OPTCR_RDP_7                   ((uint32_t)0x00008000)
+#define FLASH_OPTCR_nWRP                    ((uint32_t)0x0FFF0000)
+#define FLASH_OPTCR_nWRP_0                  ((uint32_t)0x00010000)
+#define FLASH_OPTCR_nWRP_1                  ((uint32_t)0x00020000)
+#define FLASH_OPTCR_nWRP_2                  ((uint32_t)0x00040000)
+#define FLASH_OPTCR_nWRP_3                  ((uint32_t)0x00080000)
+#define FLASH_OPTCR_nWRP_4                  ((uint32_t)0x00100000)
+#define FLASH_OPTCR_nWRP_5                  ((uint32_t)0x00200000)
+#define FLASH_OPTCR_nWRP_6                  ((uint32_t)0x00400000)
+#define FLASH_OPTCR_nWRP_7                  ((uint32_t)0x00800000)
+#define FLASH_OPTCR_nWRP_8                  ((uint32_t)0x01000000)
+#define FLASH_OPTCR_nWRP_9                  ((uint32_t)0x02000000)
+#define FLASH_OPTCR_nWRP_10                 ((uint32_t)0x04000000)
+#define FLASH_OPTCR_nWRP_11                 ((uint32_t)0x08000000)
+
+#define FLASH_OPTCR_DB1M                    ((uint32_t)0x40000000)
+#define FLASH_OPTCR_SPRMOD                  ((uint32_t)0x80000000)
+
+//
+
 #define FLASH_FLAG_BSY                 ((uint32_t)0x00010000)
 
 #define FLASH_KEY1               ((uint32_t)0x45670123)
 #define FLASH_KEY2               ((uint32_t)0xCDEF89AB)
+#define FLASH_OPT_KEY1           ((uint32_t)0x08192A3B)
+#define FLASH_OPT_KEY2           ((uint32_t)0x4C5D6E7F)
+
+#define FLASH_OPTCR_RDP_LEVEL_0 0x0000aa00
+#define FLASH_OPTCR_RDP_LEVEL_1 0x00000100
+#define FLASH_OPTCR_RDP_LEVEL_2 0x0000cc00
 
 - (void)setBits:(UInt32)address value:(UInt32)value
 {
@@ -71,7 +116,31 @@
     [self.serialWireDebug writeMemory:address value:[self.serialWireDebug readMemory:address] & ~value];
 }
 
-- (void)massErase
+- (void)replaceBits:(UInt32)address mask:(UInt32)mask value:(UInt32)value
+{
+    uint32_t replacement = [self.serialWireDebug readMemory:address];
+    replacement = (replacement & ~mask) | value;
+    [self.serialWireDebug writeMemory:address value:replacement];
+}
+
+- (void)setOptionByteReadProtection:(uint32_t)level
+{
+    if ([self.serialWireDebug readMemory:FLASH_OPTCR] & FLASH_OPTCR_OPTLOCK) {
+        [self.serialWireDebug writeMemory:FLASH_OPTKEYR value:FLASH_OPT_KEY1];
+        [self.serialWireDebug writeMemory:FLASH_OPTKEYR value:FLASH_OPT_KEY2];
+    }
+    
+    while ([self.serialWireDebug readMemory:FLASH_SR] & FLASH_FLAG_BSY);
+    
+    [self replaceBits:FLASH_OPTCR mask:FLASH_OPTCR_RDP value:level];
+    [self setBits:FLASH_OPTCR value:FLASH_OPTCR_OPTSTRT];
+    
+    while ([self.serialWireDebug readMemory:FLASH_SR] & FLASH_FLAG_BSY);
+    
+    [self setBits:FLASH_OPTCR value:FLASH_OPTCR_OPTLOCK];
+}
+
+- (void)massEraseFlash
 {
     if ([self.serialWireDebug readMemory:FLASH_CR] & FLASH_CR_LOCK) {
         [self.serialWireDebug writeMemory:FLASH_KEYR value:FLASH_KEY1];
@@ -88,6 +157,32 @@
     [self clearBits:FLASH_CR value:FLASH_CR_MER];
     
     [self setBits:FLASH_CR value:FLASH_CR_LOCK];
+}
+
+- (void)massEraseOptionByteReadProtection
+{
+    [self setOptionByteReadProtection:FLASH_OPTCR_RDP_LEVEL_0];
+}
+
+- (void)massErase
+{
+    uint32_t flash_optcr = [self.serialWireDebug readMemory:FLASH_OPTCR];
+    if ((flash_optcr & FLASH_OPTCR_RDP) != FLASH_OPTCR_RDP_LEVEL_0) {
+        [self massEraseOptionByteReadProtection];
+    } else {
+        [self massEraseFlash];
+    }
+}
+
+- (void)setDebugLock
+{
+    [self setOptionByteReadProtection:FLASH_OPTCR_RDP_LEVEL_1];
+}
+
+- (BOOL)debugLock
+{
+    uint32_t flash_optcr = [self.serialWireDebug readMemory:FLASH_OPTCR];
+    return (flash_optcr & FLASH_OPTCR_OPTLOCK) != 0;
 }
 
 @end
